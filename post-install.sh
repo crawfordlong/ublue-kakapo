96ec71d55329bc29022494bd92fc3de107b4acb3
cbl <82600093+crawfordlong@users.noreply.github.com>
2023-03-28 19:27:52 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -5,6 +5,18 @@ set -ouex pipefail
 systemctl enable rpm-ostreed-automatic.timer
 systemctl enable flatpak-system-update.timer
 
+systemctl enable tlp
+systemctl mask power-profiles-daemon.service
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket
+tlp start
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.mozilla.firefox
+flatpak install --reinstall flathub $(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
+
+flatpak uninstall f
+
 systemctl --global enable flatpak-user-update.timer
 
 cp /usr/share/ublue-os/ublue-os-update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
